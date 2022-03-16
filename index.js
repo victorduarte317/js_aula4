45aa6f6a56c81e46255425020f3c015816b0e913
Victor Duarte <83683867+victorduarte317@users.noreply.github.com>
2022-03-16 15:12:53 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * Criar uma variável com "=" indicando que um valor
+ * está sendo atribuído à tal variável.
+ */
+
+let n = 7;
+
+for (let i=0; i <= 10; i++) {
+
+    console.log(
+        `${i} X ${n} = ${i*n}` // Template String
+    ) 
+    
+    
+    // console.log(i + 'x' + n + '=' + (i*n)); Concatenação de variáveis e valores.(Modo antigo)
+
+}
---
+++ index2.js
@@ -0,0 +1,18 @@
+let cor ='vermelho';
+
+switch (cor) {
+    case 'verde':
+    console.log('siga');
+    break;
+
+    case 'amarelo':
+    console.log('atencao');
+    break;
+
+    case 'vermelho':
+    console.log('pare');
+    break;
+
+    default: 
+    console.log('tem algo de errado com seu semáforo.')
+}
